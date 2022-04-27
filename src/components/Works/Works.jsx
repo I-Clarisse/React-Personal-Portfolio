6466911d136e0d0321ddf840359fceaa68b946b2
I-Clarisse <79416565+I-Clarisse@users.noreply.github.com>
2022-04-27 21:47:03 +0200

    .

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,9 +1,37 @@
 import React from "react"
 import './Works.css'
+import Facebook from '../../images/Facebook.png'
+import Fiverr from '../../images/fiverr.png'
+import Upwork from '../../images/Upwork.png'
+import Amazon from '../../images/amazon.png'
+import Shopify from '../../images/Shopify.png'
 
 const Works = () => {
     return(
-        <div className="works"></div>
+        <div className="works">
+            <div className="awesome">
+                <span>Works for All these</span>
+                <span>Brand & Clients</span>
+                <span>
+                    Lorem ipsum dolor sit amet consectetur
+                     adipisicing elit. <br/>
+                     Dolorum aperiam odit inventore
+                      impedit vel mollitia repellendus eius?<br/>
+                       Neque, ipsa ipsam
+                       consequatur culpa natus<br/>
+                        provident quod reprehenderit voluptates, 
+                        qui assumenda nemo?
+                </span>
+                {/* download button  */}
+                    <button className="button s-button">
+                        Hire me
+                    </button>
+                {/* blur div  */}
+                <div className="blur s-blur1" style={{background: "ABF1FF94"}}></div>
+            </div>
+            {/* Right side */}
+            div.
+        </div>
     )
 }
 export default Works
