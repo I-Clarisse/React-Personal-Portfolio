68a68a09352428b7987e543e07de4b9ec697dc0a
I-Clarisse <79416565+I-Clarisse@users.noreply.github.com>
2022-04-28 16:54:56 +0200

    ....

--- src/components/Testimoniols/Testimoniols.jsx
+++ src/components/Testimoniols/Testimoniols.jsx
@@ -8,6 +8,8 @@ import Profile3 from '../../images/profile3.jpg'
 import Profile4 from '../../images/profile4.jpg'
 import Profile5 from '../../images/profile5.jpg'
 import Profile6 from '../../images/profile6.jpg'
+import { Pagination } from "swiper"
+import 'swiper/css/pagination'
 
 const Testimoniols = () => {
     const clients = [
@@ -39,18 +41,26 @@ const Testimoniols = () => {
     return(
         <div className="testimoniols">
             {/* Heading  */}
-            <span>Clients always get</span>
-            <span>Exceptional Work </span>
-            <span>from me </span>
+            <div className="t-heading">
+                <span>Clients always get</span>
+                <span>Exceptional Work </span>
+                <span>from me </span>
+            </div>
             <div className="blur t-blur1" style={{background: "var(--purple)"}}></div>
             <div className="blur t-blur2" style={{background: "skyblue"}}></div>
 
-            <Swiper>
+            <Swiper
+            modules = {[Pagination]}
+            slidesPerView = {1}
+            pagination = {{clickable: true}}
+            >
                 {clients.map((client, index) => {
                     return (
                         <SwiperSlide key={index}>
-                            <img src={client.img} alt="" />
-                            <span>{client.review}</span>
+                            <div className="testimonial">
+                                <img src={client.img} alt="" />
+                                <span>{client.review}</span>
+                            </div>
                         </SwiperSlide>
                     )
                 })}
