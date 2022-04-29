1cd06a2697bf70149eab0632d0fffe82bc3202a7
I-Clarisse <79416565+I-Clarisse@users.noreply.github.com>
2022-04-29 22:09:28 +0200

    progress

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,7 +3,24 @@ import './Contact.css'
 
 const Contact = () => {
     return(
-        <div className="contact">Contact div</div>
+        <div className="contact-form">
+            <div className="c-left">
+                <div className="awesome">
+                    <span>Get in touch</span>
+                    <span>Contact me</span>
+                    <div className="blur s-blur1" style={{background: "#ABF1FF94"}}></div>
+                </div>
+            </div>
+            <div className="c-right">
+                <form action="#">
+                    <input type="text" name="userName" className="user" placeholder="Name"/>
+                    <input type="email" name="userEmail" className="user" placeholder="Email"/>
+                    <textarea name="message" className="user" placeholder="Message"/>
+                    <input type="submit" value="Send" className="button"/>
+                    <div className="blur c-blur1" style={{background: "var(--purple)"}}></div>
+                </form>
+            </div>
+        </div>
     )
 }
 export default Contact
