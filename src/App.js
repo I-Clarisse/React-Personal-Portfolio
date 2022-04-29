3b7ba1317d6bd9fd3864095b7819211bee684216
I-Clarisse <79416565+I-Clarisse@users.noreply.github.com>
2022-04-29 21:57:24 +0200

    starting contact

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Experience from "./components/Experience/Experience";
 import Works from "./components/Works/Works";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Testimoniols from "./components/Testimoniols/Testimoniols";
+import Contact from "./components/Contact/Contact";
 import './App.css';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Works/>
       <Portfolio/>
       <Testimoniols/>
+      <Contact/>
       </div>
   );
 }
