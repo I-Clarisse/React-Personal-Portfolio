825fe17023846a994612c716f1765ff544f17585
I-Clarisse <79416565+I-Clarisse@users.noreply.github.com>
2022-04-30 21:39:09 +0200

    ....

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,20 @@
-import React from "react"
+import React, {useRef} from "react"
 import './Contact.css'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
+    const form = userRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        
+        emailjs.sendForm('service_s9x0l2r', 'template_1z164si', form.current, '6c1oV4SgqgFfAu1W5')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    };
     return(
         <div className="contact-form">
             <div className="c-left">
@@ -12,9 +25,9 @@ const Contact = () => {
                 </div>
             </div>
             <div className="c-right">
-                <form action="#">
-                    <input type="text" name="userName" className="user" placeholder="Name"/>
-                    <input type="email" name="userEmail" className="user" placeholder="Email"/>
+                <form ref={form} onSubmit={sendEmail}>
+                    <input type="text" name="user_name" className="user" placeholder="Name"/>
+                    <input type="email" name="user_email" className="user" placeholder="Email"/>
                     <textarea name="message" className="user" placeholder="Message"/>
                     <input type="submit" value="Send" className="button"/>
                     <div className="blur c-blur1" style={{background: "var(--purple)"}}></div>
