7d1358c71cead2358f3abbae96236cf3eb7b7326
I-Clarisse <79416565+I-Clarisse@users.noreply.github.com>
2022-05-01 22:16:12 +0200

    going....

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import './Footer.css'
 import Wave from '../../images/wave.png'
-import UilReact from '@iconscout/react-unicons/icons/uil-react'
+// import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import IG from '@iconscout/react-unicons/icons/uil-instagram'
 
 const Footer = () => {
     return(
         <div className="footer">
             <img src={Wave} alt="wave" style={{width: '100%'}}/>
-            <div className="f-content"></div>
+            <div className="f-content">
+                <span>clarisse2526@gmail.com</span>
+                <div className="f-icons">
+                    <IG/>
+                </div>
+            </div>
         </div>
     )
 }
